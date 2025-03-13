319e527dc2fcfb4b125ee2ba0aa9f6340b4a0b0f
Prateek Pritam <156744654+pritamprateek00@users.noreply.github.com>
2025-03-13 23:55:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,7 +69,7 @@ my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_or
 
     #st.write(my_insert_stmt)
     #st.stop()
-    time_to_insert = st.button('Submit Order')
+time_to_insert = st.button('Submit Order')
     
 if time_to_insert:
     session.sql(my_insert_stmt).collect()
